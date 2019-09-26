dd17a55c2d3aa2c134741502b5d7ee75d178260d
Eric O'Neill <oneill.eric23@gmail.com>
2019-09-26 16:08:39 -0600

    Add pseudocode and README

---
+++ README.md
@@ -0,0 +1,8 @@
+# Webscraping workshop
+
+## Overview
+This is my first attempt at building a webscraper. It scrapes fantasy.nfl.com/scoringleaders for the top 25 fantasy scoring leaders on the season and displays them in the console.
+
+## Built with
+* Node.js
+* Nightmare
--- scrape.js
+++ scrape.js
@@ -1,6 +1,10 @@
 var Nightmare = require('nightmare');
 var nightmare = Nightmare({ show: true });
 
+//go to fantasy.nfl.com/scoring leaders
+//query select all elements with the class .playerNameAndInfo
+//return the list of names and console.log them
+
 nightmare
   .goto('https://fantasy.nfl.com/research/scoringleaders')
   .wait('.playerNameAndInfo')
